ab1853d278fac2c16257bdd62e12087c1b078b48
pnjstudent <110066984+pnjstudent@users.noreply.github.com>
2022-08-09 14:49:33 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Surprised)
+basic.showIcon(IconNames.Meh)
+basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Sad)
+basic.showIcon(IconNames.Confused)
+basic.showIcon(IconNames.Asleep)
+basic.showIcon(IconNames.Silly)
+basic.showIcon(IconNames.Fabulous)
+basic.showString("Hello!,My name is joshua Whats yours?")
+basic.forever(function () {
+	
+})
