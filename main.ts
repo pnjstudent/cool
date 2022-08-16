dc7ff88be19bdf7fbce63ad3bee8e1aa94244a8d
pnjstudent <110066984+pnjstudent@users.noreply.github.com>
2022-08-16 14:28:07 -0400

    VOMIT

--- main.ts
+++ main.ts
@@ -1,10 +1,7 @@
-basic.showIcon(IconNames.SmallHeart)
-basic.showIcon(IconNames.Heart)
-basic.showIcon(IconNames.SmallHeart)
-basic.showIcon(IconNames.Heart)
-basic.showIcon(IconNames.SmallHeart)
-basic.showIcon(IconNames.Heart)
-basic.showString("Hello! My name is Joshua Coney. And I love microbit and coding. Thanks To my teacher I can do things that I can't even imagine from the start and now in the present.")
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hello! My name is Joshua Coney. And I love microbit and coding. Thanks To my teacher I can do things that I can't even imagine from the start and now in the present.")
+})
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
 })
